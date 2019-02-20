13f23b53f826fba909723445849803bc80f2e03e
LukeBarstow <stephen.barstow@westpoint.edu>
2019-02-20 11:29:34 -0500

    updating tests

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,11 +4,14 @@ from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from .models import Question, Choice
 from django.urls import reverse
+from django.utils import timezone
+
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+    
     
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
